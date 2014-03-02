d865d7cb18cb2ebd583dde926a2240607fc12eb7
Elfrey Shira <elf.shira@gmail.com>
2014-03-01 19:50:53 -0500

    move services

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,11 @@
 
 app.controller('PageController'
 app.factory('getResultsWithTitle'
-app.factory('rottenService'
-app.factory('imdbService'
-app.factory('tmdbService'
-app.factory('metacriticService'
-app.factory('imdbBackupService'
+app.factory('httpRottenService'
+app.factory('httpImdbService'
+app.factory('httpTmdbService'
+app.factory('httpMetacriticService'
+app.factory('httpImdbBackupService'
 
 
     **/
@@ -33,7 +33,7 @@ app.value('testing', function() {
 
 
 app.controller('PageController',
-    function($scope, rottenService, imdbService, tmdbService, metacriticService, imdbBackupService, $window, getResultsWithTitle) {
+    function($scope, httpRottenService, httpImdbService, httpTmdbService, httpMetacriticService, httpImdbBackupService, $window, getResultsWithTitle) {
 
         $scope.movieSearchResults = [];
 
@@ -47,27 +47,27 @@ app.controller('PageController',
 
             getResultsWithTitle(title, $scope);
 
-            // rottenService(config)
+            // httpRottenService(config)
             // .success(function(data, status) {
             //     $scope.rt = data;
             // });
 
-            // imdbService(config)
+            // httpImdbService(config)
             // .success(function(data, status) {
             //     $scope.imdb = data;
             // });
 
-            // tmdbService(config)
+            // httpTmdbService(config)
             // .success(function(data, status) {
             //     $scope.tmdb = data;
             // });
 
-            // metacriticService(config)
+            // httpMetacriticService(config)
             // .success(function(data, status) {
             //     $scope.meta = data;
             // });
 
-            // imdbBackupService(config)
+            // httpImdbBackupService(config)
             // .success(function(data, status) {
             //     console.log("success!!!!!!!!");
             //     $scope.imdb_backup = data;
@@ -150,7 +150,7 @@ $scope.movieSearchResults = [
 @returns null
 **/
 
-app.factory('getResultsWithTitle', function($q, rottenService, imdbService, tmdbService, metacriticService) {
+app.factory('getResultsWithTitle', function($q, httpRottenService, httpImdbService, httpTmdbService, httpMetacriticService) {
 
     return function getResultsWithTitle(title, scope) {
         // _.find(movies, {searchKey:2})
@@ -160,7 +160,7 @@ app.factory('getResultsWithTitle', function($q, rottenService, imdbService, tmdb
             sources: []
         };
 
-        rottenService({query: title})
+        httpRottenService({query: title})
         .success(function(data) {
 
             var rtMovieObj = data.movies[0];
@@ -198,7 +198,7 @@ app.factory('getResultsWithTitle', function($q, rottenService, imdbService, tmdb
 
 
             var imdbId = resultObj.imdbId;
-            imdbService({id: imdbId})
+            httpImdbService({id: imdbId})
             .success(function(data) {
 
                 resultObj.sources.push({
@@ -210,7 +210,7 @@ app.factory('getResultsWithTitle', function($q, rottenService, imdbService, tmdb
 
             });
 
-            tmdbService({id: imdbId})
+            httpTmdbService({id: imdbId})
             .success(function(data) {
 
                 resultObj.sources.push({
@@ -224,7 +224,7 @@ app.factory('getResultsWithTitle', function($q, rottenService, imdbService, tmdb
 
             // TODO: Find the right movie out of the results. Use lodash or something.
             // fetch('batman begins') returns 'dark knight' as first result.
-            metacriticService({query: resultObj.title})
+            httpMetacriticService({query: resultObj.title})
             .success(function(data) {
 
                 var metaMovieObj = data.results[0];
@@ -246,196 +246,6 @@ app.factory('getResultsWithTitle', function($q, rottenService, imdbService, tmdb
             });
 
         });
-};
-
-});
-
-/**
-Retrieves a JSON object from Rotten Tomatoes.
-
-@param config {Object} Must have either the 'id' or 'query' key.
-@param config.id {string} RT movie id. For example, "770805418" is Inception.
-@param config.query {string} URL-friendly search query. For example, "incepti"...
-@param? config.limit {string || "1"} Number of movie results.
-
-@returns {Promise} Follow up with 'success' or 'error'. Each function takes arguments: data, status, headers, config
-**/
-app.factory('rottenService', function($http, RT_API_KEY) {
-
-    return function rottenService(config) {
-
-        var params = {};
-        params.apiKey = RT_API_KEY;
-        params.page_limit = config.limit || '1';
-        params.callback = 'JSON_CALLBACK';
-
-        var url;
-        if (angular.isString(config.id)) {
-            url = 'http://api.rottentomatoes.com/api/public/v1.0/movies/'+ config.id +'.json';
-        }
-        else if (angular.isString(config.query)) {
-            params.q = config.query;
-            url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json';
-        }
-
-        return $http.jsonp(url, {params: params});
     };
 
-});
-
-/**
-Retrieves a JSON object from IMDB.
-
-@param config {Object} Must have either the 'id' or 'query' key.
-@param config.id {string} IMDB movie id. For example, "tt1375666" is Inception.
-@param config.query {string} URL-friendly full movie title query. For example, "inception".
-
-@returns {Promise} Follow up with 'success' or 'error'. Each function takes arguments: data, status, headers, config
-**/
-app.factory('imdbService', function($http) {
-
-    return function imdbService(config) {
-        var params = {};
-        params.callback = 'JSON_CALLBACK';
-
-        var url = 'http://www.imdbapi.com';
-        if (angular.isString(config.id)) {
-            params.i = config.id;
-        }
-        else if (angular.isString(config.query)) {
-            params.t = config.query;
-        }
-
-        return $http.jsonp(url, {params: params});      
-
-    };
-});
-
-
-
-/**
-Retrieves a JSON object from TMDB.
-
-@param config {Object} Must have either the 'id' or 'query' key.
-@param config.id {string} IMDB movie id. For example, "tt1375666" is Inception.
-@param config.query {string} URL-friendly search query. For example, "incepti"...
-
-@returns {Promise} Follow up with 'success' or 'error'. Each function takes arguments: data, status, headers, config
-**/
-app.factory('tmdbService', function($http, TMDB_API_KEY) {
-
-    return function tmdbService(config) {
-
-        var params = {};
-        params.api_key = TMDB_API_KEY;
-        params.callback = 'JSON_CALLBACK';
-
-        var url;
-        if (angular.isString(config.id)) {
-            url = 'http://api.themoviedb.org/3/movie/' + config.id;
-        }
-        else if (angular.isString(config.query)) {
-            params.query = config.query;
-            url = 'http://api.themoviedb.org/3/search/movie';
-        }
-
-        return $http.jsonp(url, {params: params});
-    };
-
-});
-
-
-/**
-Retrieves a JSON object from Metacritic using mashape.
-
-@param config {Object} Must have the 'query' key.
-@param config.query {string} URL-friendly full movie search query. For example, "inception".
-@param? config.limit {string || "1"} Maximum search result pages to crawl. (Not sure what this means.)
-
-@returns {Promise} Follow up with 'success' or 'error'. Each function takes arguments: data, status, headers, config
-**/
-app.factory('metacriticService', function($http, METACRITIC_API_KEY) {
-
-    return function metacriticService(config) {
-
-        var data = {};
-        data.max_pages = config.limit || '1';
-
-        var url = 'https://byroredux-metacritic.p.mashape.com/search/movie';
-        data.title = config.query;
-
-        var headers = {"X-Mashape-Authorization": METACRITIC_API_KEY};
-
-        return $http.post(url, data, {headers: headers});
-    };
-
-});
-
-
-
-function imdbBackupCallback(json) {
-    console.log("callback!!!!!!!!!!!!");
-    console.log(json);
-    return json;
-}
-
-// *************
-// *************
-// DO THIS!!!!!!!!! http://jsfiddle.net/Xbf6C/1/
-// *************
-
-/**
-Used only when the imdbService doesn't work. Retrieves a JSON object from IMDB.
-
-@param config {Object} Must have either the 'id' or 'query' key.
-@param config.id {string} IMDB movie id. For example, "tt1375666" is Inception.
-@param config.query {string} URL-friendly search query. For example, "inceptio"...
-@param config.year? {string} 4-digit year
-
-@returns {Promise} Follow up with 'success' or 'error'. Each function takes arguments: data, status, headers, config
-**/
-app.factory('imdbBackupService', function($http) {
-
-    return function imdbBackupService(config) {
-        var params = {};
-        params.callback = 'JSON_CALLBACK';
-        params.type = 'json';
-
-        var url = 'http://deanclatworthy.com/imdb/';
-        if (angular.isString(config.id)) {
-            params.id = config.id;
-        }
-        else if (angular.isString(config.query)) {
-            params.q = config.query;
-        }
-
-        var headers = {
-            // 'Content-Type': 'application/json'
-            'Content-Type': 'application/javascript'
-        }
-        // application/json
-        // Content-Type
-        var response = $http.jsonp(url, {params: params, headers: headers});
-
-        response.then(function(data) {
-            console.log("then data!!!!!!!!!!!!!");
-            console.log(data);
-        });
-        console.log("!!!!!!!!!!!!!");
-        console.log(response);
-        return response;
-
-
-        /**********
-        **************
-        this isn't working yet because i ran out of api usage
-        there seems to be failure in multiple ways
-        1) if i set callback to 'JSON_CALLBACK', it says it can't find the angular call back
-        2) if i take that out but keep int the json stuff,
-            it looks for some callback called 'imdbapi'. it works if i make the function globally available.
-        3) if i take out the json and callback stuff, it has the old failure of "Uncaught SyntaxError: Unexpected token : "
-        .
-        */
-
-    };
 });
