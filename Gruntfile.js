d53a71f1028142acb73a519825d36ceef9e8f00d
Elfrey Shira <elf.shira@gmail.com>
2014-03-16 18:39:38 -0400

    set up watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,31 @@
 'use strict';
 
 module.exports = function(grunt) {
+
+    var allJsFiles = ['**/*.js', '!node_modules/**'];
+
     grunt.initConfig({
+
         jshint: {
             options: {
                 node: true
             },
-            all: ['**/*.js', '!node_modules/**']
+            all: allJsFiles
+        },
+        watch: {
+            scripts: {
+                files: allJsFiles,
+                tasks: ['jshint'],
+                options: {
+                    // spawn: false
+                }
+            }
         }
+
     });
 
+    grunt.registerTask('test', ['jshint']);
+
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 };
